7e0fe556fe8ff849ba536b87b2b75828c1fc8ae7
Amin Mahrami <amin.mahrami@postnl.nl>
2024-01-22 23:27:25 +0100

    Always save the data

--- index.mjs
+++ index.mjs
@@ -1,6 +1,5 @@
 import puppeteer from "puppeteer";
 import fs from "fs";
-import _ from "lodash";
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -16,15 +15,7 @@ import _ from "lodash";
       ) {
         const freshData = await response.json();
         const archive = JSON.parse(fs.readFileSync("archive.json"));
-        const lastData = archive[archive.length - 1];
-        if (!lastData) {
-          return writeToArchive(archive, freshData);
-        }
-
-        delete lastData.scrappedAt;
-        if (!_.isEqual(lastData, freshData)) {
-          return writeToArchive(archive, freshData);
-        }
+        writeToArchive(archive, freshData);
       }
     });
 
